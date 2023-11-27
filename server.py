afca58cddc98a7a2563ea4fcf4ad181b237cc553
Andreu <andreuhuguet@gmail.com>
2023-11-27 21:29:51 +0100

    Primera versió

---
+++ server.py
@@ -0,0 +1,18 @@
+import http.server
+import socketserver
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
+        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+        super().end_headers()
+
+# Set the port
+PORT = 8000
+
+handler_object = MyHttpRequestHandler
+
+with socketserver.TCPServer(("", PORT), handler_object) as httpd:
+    print("Server started at localhost:" + str(PORT))
+    httpd.serve_forever()
+
